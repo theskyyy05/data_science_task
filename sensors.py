742123c482ea7b7e5d84e7037cb2733650fd7725
Aakash Kumar Tiwari <138760628+theskyyy05@users.noreply.github.com>
2026-01-14 18:41:22 +0530

    Enhance sensor readings processing and output
    Added functionality to calculate and print the highest total signal from the readings.

--- sensors.py
+++ sensors.py
@@ -1,9 +1,12 @@
+
+
 n=int(input("enter the no of readings "))
 readings=eval(input("enter the readings of values"))
 k=int(input("enter the times of reading"))
 l1=[]
 l2=[]
 i=0
+
 for i in range(2):
   i=0
   while i<=2:
@@ -12,6 +15,15 @@ for i in range(2):
     i=i+1
   i=i+1
 l2.append(l1)
+sum=0
 print(l2)
+
+for i in l2:
+  for j in i:
+    sum=sum+j
+
+list_of_sum=list(sum)
+print(f'the highest toral signal is{max(list_of_sum)} ')
+
 
 
