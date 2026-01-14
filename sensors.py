57f2fecc4ec67c83e90118726513c455a8a51dc0
Aakash Kumar Tiwari <138760628+theskyyy05@users.noreply.github.com>
2026-01-14 18:20:40 +0530

    data science intern task submission

---
+++ sensors.py
@@ -0,0 +1,17 @@
+n=int(input("enter the no of readings "))
+readings=eval(input("enter the readings of values"))
+k=int(input("enter the times of reading"))
+l1=[]
+l2=[]
+i=0
+for i in range(2):
+  i=0
+  while i<=2:
+    l1.append(readings[i]) 
+    list(l1)
+    i=i+1
+  i=i+1
+l2.append(l1)
+print(l2)
+
+
